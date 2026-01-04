b359047077ef0f0ab662f19e6a4b488254836a0d
3manuel0 <saiddimension@gmail.com>
2026-01-04 02:09:31 +0100

    fwrite/write & NULL checking

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int main(){
     printf("cap:%zu len:%zu p:%p\n", s.cap, s.len, s.str);
     string_b_push_string_v(&s, &string_v_from_lit(" anger builds"));
     printf("cap:%zu len:%zu p:%p\n", s.cap, s.len, s.str);
-    string_b_println(&s);
+    for(int i = 0; i < 100000; i++)
+        string_b_println(&s);
+    // 310 <-> 298 write
+    // 182 <-> 160 fwrite fwrite is faster 
     return 0;
 }
--- src/string.c
+++ src/string.c
@@ -1,6 +1,4 @@
 #include "../includes/lib3man.h"
-#include <stddef.h>
-#include <string.h>
 
 // TODO: string_v inside areanaList
 
@@ -20,23 +18,42 @@ int string_v_cmp(const string_v *s1, const string_v *s2){
 }
 
 void string_v_println(const string_v *s){
-    write(1, (char *)s->str, s->len);
-    write(1, "\n", 1);
+    if(s->str == NULL){
+        fwrite("empty\n", 1, 6, stdout);
+        return;
+    }
+    fwrite(s->str, 1, s->len,stdout);
+    fwrite("\n", 1, 1,stdout);
+    // write(1, (char *)s->str, s->len); // write is too slow no buffering
+    // write(1, "\n", 1);
 }
 
 void string_v_print(const string_v *s){
-    write(1, (char *)s->str, s->len);
+    if(s->str == NULL){
+        fwrite("empty", 1, 5, stdout);
+        return;
+    }
+    fwrite(s->str, 1, s->len,stdout);
+    // write(1, (char *)s->str, s->len); // write is too slow no buffering
 }
 
 
 // string buffer functions ##################################################################
+
+// TODO: string_b inside areana and arenaList
+
 // creating a string_b from char *
-string_b create_string_b_fchar(const char *s){
+string_b string_b_fchar(const char *s){
     if(s == NULL) return (string_b){.str = NULL, .len = 0, .cap = 0};
 
     size_t len = strlen(s);
     size_t cap = len * 4;
     char *temp = malloc(cap);
+
+    if(temp == NULL){
+        fprintf(stderr, "Error, Allocation Failed");
+        return (string_b){.str = NULL, .len = 0, .cap = 0};
+    }
 
     return (string_b){.str = temp, .len = len, .cap = cap};
 }
@@ -46,6 +63,12 @@ string_b string_b_fstring_v(const string_v *s){
         return (string_b){.str = NULL, .len = 0, .cap = 0};
 
     char * temp = malloc(s->len * 4);
+
+    if(temp == NULL){
+        fprintf(stderr, "Error, Allocation Failed");
+        return (string_b){.str = NULL, .len = 0, .cap = 0};
+    }
+
     memcpy(temp, s->str, s->len);
     
     return (string_b){.str = temp, .len = s->len, .cap = s->len*4};
@@ -153,13 +176,13 @@ int string_b_push_sstr(string_b *sb, const char *str, size_t size){
         sb->len += size;
     }else{
         size_t temp_cap = sb->cap * 2;
-        char * s  = realloc(sb->str, sb->cap);
-        if(s == NULL){
+        char * temp  = realloc(sb->str, sb->cap);
+        if(temp == NULL){
             fprintf(stderr, "Erorr, Realocation Failed\n");
             return str_fail;
         }
         sb->cap = temp_cap;
-        sb->str = s;
+        sb->str = temp;
         memcpy(sb->str+sb->len, str, size);
         sb->len += size;
     }
@@ -169,19 +192,22 @@ int string_b_push_sstr(string_b *sb, const char *str, size_t size){
 
 void string_b_println(const string_b *s){
     if(s->str == NULL){
-        write(1, "empty\n", 6);
+        // write(1, "empty\n", 6);
+        fwrite("empty\n", 1, 6, stdout);
         return;
     }
-    write(1, s->str, s->len);
-    write(1, "\n", 1);
+    fwrite(s->str, 1, s->len,stdout);
+    fwrite("\n", 1, 1,stdout);
 }
 
 void string_b_print(const string_b *s){
     if(s->str == NULL){
-        write(1, "empty", 5);
+        // write(1, "empty", 5);
+        fwrite("empty", 1, 5, stdout);
         return;
     }
-    write(1, s->str, s->len);
+    // write(1, s->str, s->len);
+    fwrite(s->str, 1, s->len,stdout);
 }
 
 void string_b_free(string_b *dest){
